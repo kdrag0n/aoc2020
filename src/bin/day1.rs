e8efa42c516706002b6e4f236ffba3d99130b45d
Danny Lin <danny@kdrag0n.dev>
2020-12-01 19:29:27 -0800

    Add naïve solution for day 1

---
+++ src/bin/day1.rs
@@ -0,0 +1,17 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let mut numbers = Vec::new();
+    for line in io::stdin().lock().lines() {
+        numbers.push(line.unwrap().parse::<i32>().unwrap());
+    }
+
+    'outer: for x in numbers.iter() {
+        for y in numbers.iter() {
+            if x + y == 2020 {
+                println!("{}", x * y);
+                break 'outer;
+            }
+        }
+    }
+}
