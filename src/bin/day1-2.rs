8f8df4f97e01f498c34cc63720b645d2a4015061
Danny Lin <danny@kdrag0n.dev>
2020-12-01 19:51:53 -0800

    Optimize day 1 part 2 solution

--- src/bin/day1-2.rs
+++ src/bin/day1-2.rs
@@ -1,18 +1,21 @@
 use std::io::{self, BufRead};
+use std::collections::HashSet;
 
 fn main() {
-    let mut numbers = Vec::new();
+    let mut nums_vec = Vec::new();
+    let mut nums_set = HashSet::new();
     for line in io::stdin().lock().lines() {
-        numbers.push(line.unwrap().parse::<i32>().unwrap());
+        let num = line.unwrap().parse::<i32>().unwrap();
+        nums_vec.push(num);
+        nums_set.insert(num);
     }
 
-    'outer: for x in numbers.iter() {
-        for y in numbers.iter() {
-            for z in numbers.iter() {
-                if x + y + z == 2020 {
-                    println!("{}", x * y * z);
-                    break 'outer;
-                }
+    'outer: for x in nums_vec.iter() {
+        for y in nums_vec.iter() {
+            let candidate = 2020 - (x + y);
+            if nums_set.contains(&candidate) {
+                println!("{}", x * y * candidate);
+                break 'outer;
             }
         }
     }
