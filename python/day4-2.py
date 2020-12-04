f14c1516a7be091b52b347a35be746cae0f0caa4
Danny Lin <danny@kdrag0n.dev>
2020-12-03 22:43:33 -0800

    Split day 4 parts

---
+++ python/day4-2.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+import sys
+
+def ints(itr):
+    return [int(i) for i in itr]
+
+with open(sys.argv[1], "r") as f:
+    lines = [l for l in f.read().split("\n")]
+
+
+
+ilist = []
+imap = {}
+
+total = 0
+result = 0
+other = 0
+
+while True:
+    fields = {}
+    def val():
+        for k in ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]:
+            if k not in fields:
+                return False
+            x = int(fields.get("byr", "0"))
+            if not (x >= 1920 and x <= 2002):
+                return False
+            x = int(fields.get("iyr", "0"))
+            if not (x >= 2010 and x <= 2020):
+                return False
+            x = int(fields.get("eyr", "0"))
+            if not (x >= 2020 and x <= 2030):
+                return False
+            x = fields.get("hgt", "0asxc")
+            if x.endswith("cm"):
+                y= int(x.replace("cm","")) 
+                if not (y >= 150 and y <= 193):
+                    return False
+            elif x.endswith("in"):
+                y= int(x.replace("in","")) 
+                if not (y >= 59 and y <= 76):
+                    return False
+            else:
+                return False
+            x = fields.get("ecl", "dasdd")
+            if x not in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]:
+                return False
+            x = fields.get("hcl", "ASD")
+            if not x.startswith("#"):
+                return False
+            if len(x) != 7:
+                return False
+            try:
+                int(x[1:], 16)
+            except ValueError:
+                return False
+            x = fields.get("pid", "SAD")
+            if len(x) != 9:
+                return False
+            try:
+                int(x)
+            except ValueError:
+                return False
+        return True
+    for l in lines:
+        if not l:
+            if val():
+                total += 1
+            fields = {}
+            continue
+        
+        if l.count(":") > 1:
+            for tk in l.split(" "):
+                print(tk)
+                k, v=tk.split(":")
+                fields[k]=v
+        else:
+            k, v=l.split(":")
+            fields[k] = v
+
+
+    if fields:
+        if val():
+            total += 1
+    break
+
+
+
+print(f"Total: {total}")
+print(f"Result: {result}")
+print(f"Other: {other}")
--- python/day4.py
+++ python/day4.py
@@ -20,47 +20,9 @@ other = 0
 while True:
     fields = {}
     def val():
+        print(fields)
         for k in ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]:
             if k not in fields:
-                return False
-            x = int(fields.get("byr", "0"))
-            if not (x >= 1920 and x <= 2002):
-                return False
-            x = int(fields.get("iyr", "0"))
-            if not (x >= 2010 and x <= 2020):
-                return False
-            x = int(fields.get("eyr", "0"))
-            if not (x >= 2020 and x <= 2030):
-                return False
-            x = fields.get("hgt", "0asxc")
-            if x.endswith("cm"):
-                y= int(x.replace("cm","")) 
-                if not (y >= 150 and y <= 193):
-                    return False
-            elif x.endswith("in"):
-                y= int(x.replace("in","")) 
-                if not (y >= 59 and y <= 76):
-                    return False
-            else:
-                return False
-            x = fields.get("ecl", "dasdd")
-            if x not in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]:
-                return False
-            x = fields.get("hcl", "ASD")
-            if not x.startswith("#"):
-                return False
-            if len(x) != 7:
-                return False
-            try:
-                int(x[1:], 16)
-            except ValueError:
-                return False
-            x = fields.get("pid", "SAD")
-            if len(x) != 9:
-                return False
-            try:
-                int(x)
-            except ValueError:
                 return False
         return True
     for l in lines:
@@ -70,7 +32,7 @@ while True:
             fields = {}
             continue
         
-        if l.count(":") > 1:
+        if c.count(":") > 1:
             for tk in l.split(" "):
                 print(tk)
                 k, v=tk.split(":")
